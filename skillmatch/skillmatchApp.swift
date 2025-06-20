ff7845fb4d8451ef21603c024b3f4d998932a2ef
Yusuke Nakamura <yu9su1ke1@gmail.com>
2025-06-20 10:11:06 +0500

    Initial Commit

---
+++ skillmatch/skillmatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  skillmatchApp.swift
+//  skillmatch
+//
+//  Created by 中村優介 on 2025/06/20.
+//
+
+import SwiftUI
+
+@main
+struct skillmatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
